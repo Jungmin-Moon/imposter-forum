d2ec89412787afc40bc83444f12202da60651686
Jungmin Moon <jmmoon96@gmail.com>
2024-05-31 00:23:37 -0400

    Initial Commit

---
+++ src/databaseconnector/Connector.java
@@ -0,0 +1,29 @@
+package databaseconnector;
+import java.sql.*;
+
+
+public class Connector {
+
+    Connection connection = null;
+
+    protected Connection establishConnection () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/employee_information",
+                    "luckyseven", "L}1h0#40iF40xEjYFB"
+            );
+
+            Statement statement = connection.createStatement();
+        }
+        catch (Exception exception) {
+            System.out.println(exception);
+        }
+
+        return connection;
+    }
+
+    protected void closeConnection() throws SQLException{
+        connection.close();
+    }
+}
