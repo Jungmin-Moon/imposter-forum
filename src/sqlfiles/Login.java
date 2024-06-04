82a41852c5a479b823b105d0cc11747e93c48f47
Jungmin Moon <jmmoon96@gmail.com>
2024-06-04 02:04:00 -0400

    Started on Login file as well as a simple User file. User file will help correct user name to persist between scenes.

--- src/sqlfiles/Login.java
+++ src/sqlfiles/Login.java
@@ -1,5 +1,24 @@
 package sqlfiles;
+import java.sql.*;
 
 public class Login {
 
+
+
+
+
+    public boolean validateUserName(String userName, Connection conn) {
+        boolean correctUser = false;
+
+
+        return correctUser;
+    }
+
+    public boolean validatePassword(String password, Connection conn) {
+        boolean correctPass = false;
+
+
+
+        return correctPass;
+    }
 }
---
+++ src/userfile/User.java
@@ -0,0 +1,16 @@
+package userfile;
+
+public class User {
+
+    private String userName;
+
+    User (String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+
+}
