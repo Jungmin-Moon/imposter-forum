5ac790f22ed7922de7f154621fe6495f6352a52c
Jungmin Moon <jmmoon96@gmail.com>
2024-06-21 00:08:45 -0400

    Work on the thread scene, with next time work on getting the text correctly put in their respective tables and being to retrieve and go to them in the application.

--- src/scenes/Thread_List_Scene.java
+++ src/scenes/Thread_List_Scene.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import sqlfiles.Thread_List;
@@ -23,13 +24,18 @@ public class Thread_List_Scene {
         BorderPane threadPane = new BorderPane();
         GridPane threadsMiddle = new GridPane();
 
+        Scene threadScene = new Scene(threadPane, 600, 600);
+
         threadPane.setCenter(threadsMiddle);
 
         Label placeHolder = new Label();
 
         Label sceneTitle = new Label();
 
-        Text spacing = new Text("==========");
+        Text spacing = new Text("          ");
+
+        Hyperlink title = null;
+
 
         if (listOfThreads.isEmpty()) {
             placeHolder.setText("There are currently no threads.");
@@ -38,16 +44,51 @@ public class Thread_List_Scene {
             sceneTitle.setText("====================Threads====================");
             sceneTitle.setAlignment(Pos.CENTER);
             for (int i = 0; i < listOfThreads.size(); i++) {
-                Hyperlink title = new Hyperlink(listOfThreads.get(i).getName());
+                title = new Hyperlink(listOfThreads.get(i).getName());
                 Text madeByUser = new Text(listOfThreads.get(i).getWhoStarted());
                 Text dateMade = new Text(listOfThreads.get(i).getDateCreated().toString());
                 threadsMiddle.addRow(i, madeByUser, spacing, title, spacing, dateMade);
             }
+
+
         }
 
+
         Button createThread = new Button("Create new thread");
+        GridPane bottomPane = new GridPane();
 
-        return new Scene(threadPane, 600, 600);
+        bottomPane.addRow(0, createThread);
 
+        createThread.setOnAction(e -> threadPane.setCenter(createThreadPane(threadPane, threadsMiddle, conn)));
+
+
+        return threadScene;
     }
+
+    public static Pane createThreadPane(BorderPane tPane, GridPane threads, Connection conn) {
+        GridPane creatingThread = new GridPane();
+
+        Label title = new Label("Title of thread");
+        Label body = new Label("Enter text here:");
+
+        TextField titleField = new TextField();
+        TextArea post = new TextArea();
+
+        Button submitThread = new Button("Submit");
+
+        Text space = new Text(" ");
+
+        post.setTextFormatter(new TextFormatter<String>(change ->
+                            change.getControlNewText().length() <= 400 ? change : null));
+
+        creatingThread.addRow(0, title, space, titleField);
+        creatingThread.addRow(3, body);
+        creatingThread.addRow(4, post);
+        creatingThread.addRow(6, submitThread);
+
+        submitThread.setOnAction(e -> tPane.setCenter(threads));
+
+        return creatingThread;
+    }
+
 }
